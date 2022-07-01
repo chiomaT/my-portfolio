8a7fc6269128560caf988848c962ae138142ec91
chiomaT <chiomanwabugwu11@yahoo.com>
2022-07-01 14:21:16 +0100

    my first commit/ input

---
+++ src/components/InputField.jsx
@@ -0,0 +1,10 @@
+const InputField = (props) => {
+    const { handleChange, label, name, type, value, placeholder } = props;
+    return (
+      <div className="mb-5">
+        <input className=" outline-none text-gray-700 w-full bg-gray-100 mt-4 rounded-lg p-3" type={type} onChange={handleChange} value={value} name={name} required placeholder={placeholder} />
+      </div>
+    )
+  }
+  
+  export default InputField
